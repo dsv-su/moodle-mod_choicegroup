977801f2a961ae56c207ac4aa8c7222e8c60a41e
Anton <anton@dsv.su.se>
2025-01-07 14:59:50 +0100

    Test 1

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2024111301;
-$plugin->requires  = 2022112800; // Moodle 4.1
+$plugin->requires  = 2022112800; // Moodle 4.1.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'mod_choicegroup';
